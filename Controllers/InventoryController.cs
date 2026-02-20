2509b73ca6ae0ebc2ded694487a3f352239f2a6a
Ruslan <gaffarovruslan77@gmail.com>
2026-02-20 22:24:00 +0500

    Fix inventory creation with real user

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using InventoryApp.Web.Data;
 using InventoryApp.Web.Models;
 
@@ -8,10 +10,12 @@ namespace InventoryApp.Web.Controllers;
 public class InventoryController : Controller
 {
     private readonly AppDbContext _db;
+    private readonly UserManager<AppUser> _userManager;
 
-    public InventoryController(AppDbContext db)
+    public InventoryController(AppDbContext db, UserManager<AppUser> userManager)
     {
         _db = db;
+        _userManager = userManager;
     }
 
     public async Task<IActionResult> Index()
@@ -24,20 +28,25 @@ public class InventoryController : Controller
         return View(inventories);
     }
 
-    [HttpGet]
+    [Authorize]
     public IActionResult Create()
     {
         return View();
     }
 
-    [HttpPost]
+    [HttpPost, Authorize]
     public async Task<IActionResult> Create(string title, string? description)
     {
+        var userId = _userManager.GetUserId(User);
+        
+        if (string.IsNullOrEmpty(userId))
+            return Content("User ID is null - not logged in properly");
+
         var inventory = new Inventory
         {
             Title = title,
             Description = description,
-            CreatorId = "temp",
+            CreatorId = userId,
             CreatedAt = DateTime.UtcNow,
             UpdatedAt = DateTime.UtcNow
         };
